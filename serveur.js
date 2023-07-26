ddc89f8ffcb7a6d1600fb24f222e24f6ca745a37
Rabbivoka9 <rabbivoka9@gmail.com>
2023-07-26 20:50:33 +0200

    serveur

--- serveur.js
+++ serveur.js
@@ -2,10 +2,12 @@
 import express from "express"
 import router from "./route/routeCreaEvents.js"
 import Events from "./Models/modelEvents.js"
+import cors from "cors"
 // const router = require ("./routeLogin")
 
 const app = express()
+app.use(cors())
 
 app.use("/",router)
 await Events.sync(); // synchronisation de la base des donnees
-app.listen(5000) //definition du port
+app.listen(4000) //definition du port
