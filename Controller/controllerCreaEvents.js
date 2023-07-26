63d35845a9c53c74f3814a0112175d1ea8039900
Rabbivoka9 <rabbivoka9@gmail.com>
2023-07-26 19:30:42 +0200

    controleur creation evenenement

--- Controller/controllerCreaEvents.js
+++ Controller/controllerCreaEvents.js
@@ -1,9 +1,28 @@
-
-export function AffEvent(req, res){
+import Events from "../Models/modelEvents.js";
+export const AffEvent = (req, res) =>{
        res.send('Affichange')
 }
 
 
-export function ReqEvent (req,res){
-       res.send ('Recuperation')
+export const ReqEvent = async (req,res)=>{
+       const {titreevenent,photofond, titregeneral, titredescription,description,imageprofil, titre1, titre2} = req.body;
+
+       if (titreevenent===""||photofond===""||titregeneral===""||titredescription===""||description===""||imageprofil===""||titre1===""||titre2===""){
+              return  res.status(500).json ({status:'veuillez remplir tout les champs'})
+         }
+
+         const  PlanEvenement = await Events.create({
+              titreevenent,
+              photofond,
+              titregeneral,
+              titredescription,
+              description,
+              imageprofil,
+              titre1,
+              titre2
+       })
+       
+       return res.status(200).json ({PlanEvenement})
+
+      
 }
