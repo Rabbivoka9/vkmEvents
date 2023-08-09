3fd53bd20ae509f07f12a6151eadeff26300e69d
Rabbivoka9 <rabbivoka9@gmail.com>
2023-08-09 10:04:34 +0200

    mercredi

--- Controller/controllerCreaEvents.js
+++ Controller/controllerCreaEvents.js
@@ -19,15 +19,14 @@ export const AffEvent = async(req, res) =>{
 
 
 export const ReqEvent = async (req, res) => {
-  const { idUser, titreevenent, titregeneral, titredescription, description,  titre1, titre2 } = req.body;
+  const { idUser, titreevenent, titregeneral, titredescription, description,  titre1, titre2,soust1,soust2,soust3,soust4,soust5,soust6,soust7,soust8 } = req.body;
   // Vérifier si toutes les propriétés sont remplies
-  if (titreevenent === "" ||titregeneral === "" || titredescription === "" || description === "" || titre1 === "" || titre2 === "" || idUser ==="") {
+  if (titreevenent === "" ||titregeneral === "" || titredescription === "" || description === "" || titre1 === "" || titre2 === "" || idUser ===""||soust1===""||soust2 ===""||soust3 ===""||soust4===""||soust5===""||soust6===""||soust7===""||soust8==="") {
     return res.status(500).json({ status: 'veuillez remplir tout les champs' });
   }
 
-
-
-  // Utiliser l'ID généré pour créer l'événement
+  try {
+     // Utiliser l'ID généré pour créer l'événement
   const PlanEvenement = await Events.create({
     
     idUser,
@@ -36,6 +35,14 @@ export const ReqEvent = async (req, res) => {
     titredescription,
     description,
     titre1,
+    soust1,
+    soust2,
+    soust3,
+    soust4,
+    soust5,
+    soust6,
+    soust7,
+    soust8,
     titre2
   });
 
@@ -45,7 +52,22 @@ export const ReqEvent = async (req, res) => {
     titredescription:PlanEvenement.titredescription,
     description:PlanEvenement.description,
     titre1:PlanEvenement.titre1,
-    titre2:PlanEvenement.titre2
+    titre2:PlanEvenement.titre2,
+    soust1:PlanEvenement.soust1,
+    soust2:PlanEvenement.soust2,
+    soust3:PlanEvenement.soust3,
+    soust4:PlanEvenement.soust4,
+    soust5:PlanEvenement.soust5,
+    soust6:PlanEvenement.soust6,
+    soust7:PlanEvenement.soust7,
+    soust8 : PlanEvenement.soust8
   });
+    
+  } catch (error) {
+    console.log("error", error)
+  }
+
+
+ 
 }
 
